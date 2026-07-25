dfd4caf435d232bbb004c64841c1326a224747b6
matheuspereirafx <matheuspereirafx@gmail.com>
2026-07-24 23:35:19 -0300

    Pagina de cadastro - Termo validacao

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :terms
+
+  validates :terms,
+            acceptance: {
+              message: "Aceite os Termos de Serviço e a Política de Privacidade."
+            }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

936eaa2ffbe29a7f5b8180e53728c2b776c17f99
matheuspereirafx <matheuspereirafx@gmail.com>
2026-08-27 02:57:49 -0300

    Show torneio localizacao reposicionamento e centralizacao da div header

--- test/controllers/tournaments_controller_test.rb
+++ test/controllers/tournaments_controller_test.rb
@@ -128,21 +128,25 @@ class TournamentsControllerTest < ActionDispatch::IntegrationTest
     assert_select ".overview-header__status", count: 0
   end
 
-  test "only non-players see the link back to the club" do
+  test "members see the link appropriate to their tournament navigation" do
     tournament = create_tournament(@club)
 
     [@owner, @admin, @dealer].each do |user|
       sign_in user
       get club_tournament_path(@club, tournament)
 
-      assert_select ".overview-header__back", text: "← Voltar ao clube", count: 1
+      assert_select ".overview-header__back[href='#{club_path(@club)}']",
+                    text: "← Voltar ao clube",
+                    count: 1
       sign_out user
     end
 
     sign_in @player
     get club_tournament_path(@club, tournament)
 
-    assert_select ".overview-header__back", count: 0
+    assert_select ".overview-header__back[href='#{player_tournaments_path}']",
+                  text: "← Voltar aos torneios",
+                  count: 1
   end
 
   test "a member cannot view a tournament from another club by changing its id" do

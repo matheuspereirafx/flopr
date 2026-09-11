d49f897c32add8dcb1ecb24eea8fc2b733570af0
matheuspereirafx <matheuspereirafx@gmail.com>
2026-09-10 22:32:41 -0300

    fix (torunaments): NameClub Card Tournaments

--- test/controllers/player_tournaments_controller_test.rb
+++ test/controllers/player_tournaments_controller_test.rb
@@ -35,6 +35,12 @@ class PlayerTournamentsControllerTest < ActionDispatch::IntegrationTest
     assert_select ".player-tournaments-page__section .club-events__filter[data-filter='upcoming']",
                   count: 2
     assert_select ".player-tournaments-page__section:nth-of-type(1) .tournament-card__title", 2
+    assert_select ".player-tournaments-page__section:nth-of-type(1) .tournament-card__club-name",
+                  text: @club.name,
+                  count: 1
+    assert_select ".player-tournaments-page__section:nth-of-type(1) .tournament-card__club-name",
+                  text: second_membership_club.name,
+                  count: 1
     assert_select ".player-tournaments-page__section .tournament-card__link[href*='join=true']", count: 2
     assert_operator response.body.index(earliest.name), :<, response.body.index(latest.name)
     assert_not_includes response.body, "Private Tournament"
